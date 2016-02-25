ab78b54d49eee6908a6326d7e15b19aa06a6052a
ZKINGBOTZ <ZKINGBOTZ@gmail.com>
2016-02-25 12:20:11 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+   "admin"
+   "linkpv"
     },
     sudo_users = {90971889,121739745,170712721,112703661},--Sudo users
     disabled_channels = {},
@@ -350,6 +351,8 @@ Demote someone
 
 !kickme
 Will kick user
+
+!linkpy
 
 !about
 Group description
